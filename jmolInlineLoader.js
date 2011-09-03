df709708d4ac90e801124cfb07b4597c1cb1cc67
Anton Petrov <anton.i.petrov@gmail.com>
2011-09-03 17:40:10 -0400

    Navigation bug fixes and improved options

--- jmolInlineLoader.js
+++ jmolInlineLoader.js
@@ -15,7 +15,11 @@ var jmolInlineLoader = function(){
 	// defaults are updated in the "initialize" function
     var defaults = {
 		serverUrl   : 'http://rna.bgsu.edu/research/anton/MotifAtlas/nt_coord.php',
-		chbxClass   : ''
+		chbxClass   : '',
+		neighborhoodButtonId: '',
+		showNextButtonId: '',
+		showPreviousButtonId: '',
+		showAllButtonId: ''
     };
 
 	// "Private" class. Stores the state of each checkbox.
@@ -35,8 +39,8 @@ var jmolInlineLoader = function(){
 			$.post(settings.serverUrl, { 'model': nts },
 				function(data) {
 					jmolScript("load DATA \"append structure\"\n" + data + 'end "append structure";');
-					self.superimpose();
-					self.style();
+//					self.superimpose();
+//					self.style();
 					self.display();
 			});
 		}
@@ -45,7 +49,6 @@ var jmolInlineLoader = function(){
 		this.superimpose = function () {
 			var m = self.modelNumber;
 			if ( m > 1 ) {
-				//jmolScript('compare {' + m + '.1} {1.1} 0 rotate translate;');
 				for (var i = 0; i < 2; i++) {
 					jmolScript('x=compare({*.P/' + m + '.1},{*.P/1.1});');
 					jmolScript('select ' + m + '.0; rotate selected @{x};');
@@ -56,12 +59,13 @@ var jmolInlineLoader = function(){
 		// apply styling to the model
 		this.style = function () {
 			var m = self.modelNumber;
-		 	jmolScript('select [U]/' + m + '.0; color navy;');
-		 	jmolScript('select [G]/' + m + '.0; color chartreuse;');
-		 	jmolScript('select [C]/' + m + '.0; color gold;');
-			jmolScript('select [A]/' + m + '.0; color red;');
+		 	jmolScript('select [U]/' + m + '.1; color navy;');
+		 	jmolScript('select [G]/' + m + '.1; color chartreuse;');
+		 	jmolScript('select [C]/' + m + '.1; color gold;');
+			jmolScript('select [A]/' + m + '.1; color red;');
+			jmolScript('select */' + m + '.2; color grey;');
 			jmolScript('select ' + m + '.2; color translucent 0.8;');
-			jmolScript('select *;frame *;spacefill off;center 1.1;');
+			jmolScript('select ' + m + '.0;spacefill off;center 1.1;');
 		}
 
 		this.set_neighborhood = function (neighborhood) {
@@ -83,13 +87,14 @@ var jmolInlineLoader = function(){
 		this.toggle_view = function () {
 
 			if (self.checked) {
-				// show
+				// show everything
 				if (self.neighborhood) {
 					jmolScript('frame *;display displayed or ' + self.modelNumber + '.0;');
 				} else {
 					jmolScript('frame *;display displayed or ' + self.modelNumber + '.1;');
 					jmolScript('frame *;display displayed and not ' + self.modelNumber + '.2;');
 				}
+				jmolScript('center 1.1;');
 			} else {
 				// hide everything
 				jmolScript('frame *;display displayed and not ' + self.modelNumber + '.0;');
@@ -125,6 +130,17 @@ var jmolInlineLoader = function(){
 		}
 
 	}
+	
+	this.superimpose_all = function () {
+		
+		for (id in models) {
+			models[id].superimpose();
+			models[id].style();
+			models[id].toggle_view();
+		}
+		jmolScript('center 1.1;');
+		
+	}	
 
 	this.toggle_neighborhood = function() {
 
@@ -149,7 +165,33 @@ var jmolInlineLoader = function(){
 			settings = $.extend( {}, defaults, options );
 		}
    		jmolScript('set baseCartoonEdges = true;');
+
+		$(document).ajaxStop(function() {
+			superimpose_all();			  
+		});
+
+   		settings.chbxClass = '.' + settings.chbxClass;
+   		$(settings.chbxClass).click(jmolInlineLoader.checkbox_click);
    		checkbox_click( $(settings.chbxClass).first().attr('id') );
+
+   		// attach event handlers if ids have been provided
+   		if ( settings.neighborhoodButtonId != '' ) {
+   			settings.neighborhoodButtonId = '#' + settings.neighborhoodButtonId;
+	   		$(settings.neighborhoodButtonId).click(jmolInlineLoader.toggle_neighborhood);
+   		}
+   		if ( settings.showNextButtonId != '' ) {
+   			settings.showNextButtonId = '#' + settings.showNextButtonId;
+	   		$(settings.showNextButtonId).click(jmolInlineLoader.show_next);
+   		}
+   		if ( settings.showPreviousButtonId != '' ) {
+   			settings.showPreviousButtonId = '#' + settings.showPreviousButtonId;
+	   		$(settings.showPreviousButtonId).click(jmolInlineLoader.show_prev);
+   		}
+   		if ( settings.showAllButtonId != '' ) {
+   			settings.showAllButtonId = '#' + settings.showAllButtonId;
+	   		$(settings.showAllButtonId).click(jmolInlineLoader.toggle_all);
+   		}
+
 	}
 
 	this.show_next = function() {
@@ -159,54 +201,73 @@ var jmolInlineLoader = function(){
 		for (var i=0;i<chbx.length-1;i++) {
 			if ( chbx[i].checked ) {
 				toCheck.push(i+1);
+				checkbox_click(chbx[i].id);
 			}
 		}
-		$(settings.chbxClass+':checked:not').removeAttr('checked');
+		if ( chbx[chbx.length-1].checked ) {
+			toCheck.push(0);
+			checkbox_click(chbx[chbx.length-1].id);
+		}		
+		$(settings.chbxClass + ':checked').removeAttr('checked');
+
 		for (i=0;i<toCheck.length;i++) {
 			chbx[toCheck[i]].checked = true;
+			checkbox_click(chbx[toCheck[i]].id);
 		}
-		if ( $('.jmolInline:checked').length == 0 ) {
+		if ( $(settings.chbxClass + ':checked').length == 0 ) {
 			chbx[0].checked = true;
+			checkbox_click(chbx[0].id);
 		}
 
-	}
-
-	this.toggle_all = function () {
-
-		var all = $(settings.chbxClass);
-		if ( all.length == $(settings.chbxClass+':checked').length ) {
-			// hide all
-			this.value = 'Show all';
-			all.each( function(i) {
-				this.checked = false;
-			});
-		} else {
-			// show all
-			this.value = 'Hide all';
-			all.each( function(i) {
-				this.checked = true;
-			});
-		}
 	}
 
 	this.show_prev = function () {
 
 		var chbx = $(settings.chbxClass);
 		var toCheck = new Array();
+		// loop over all checkboxes except for the first one
 		for (var i=chbx.length-1;i>=1;i--) {
 			if ( chbx[i].checked ) {
 				toCheck.push(i-1);
+				checkbox_click(chbx[i].id);
 			}
 		}
+		// separate handling of the first checkbox
+		if ( chbx[0].checked ) {
+			toCheck.push(chbx.length-1);
+			checkbox_click(chbx[0].id);
+		}		
 		// temporarily uncheck everything
-		$(settings.chbxClass+':checked:not').removeAttr('checked');
+		$(settings.chbxClass+':checked').removeAttr('checked');
 		// check only the right ones
 		for (i=0;i<toCheck.length;i++) {
 			chbx[toCheck[i]].checked = true;
+			checkbox_click(chbx[toCheck[i]].id);
 		}
-		// keep the last checkbox checked
+		// keep the last checkbox checked if all others are unchecked
 		if ( $(settings.chbxClass+':checked').length == 0 ) {
 			chbx[chbx.length-1].checked = true;
+			checkbox_click(chbx[chbx.length-1].id);
+		}
+	}
+
+	this.toggle_all = function () {
+
+		var all = $(settings.chbxClass);
+		if ( all.length == $(settings.chbxClass + ':checked').length ) {
+			// hide all
+			this.value = 'Show all';
+			all.each( function(i) {
+				this.checked = false;
+				checkbox_click(this.id);
+			});
+		} else {
+			// show all
+			this.value = 'Hide all';
+			$(settings.chbxClass+':not(:checked)').each( function(i) {
+				this.checked = true;
+				checkbox_click(this.id);				
+			});
 		}
 	}
 
