1059add2de597f1dbebdb2b4c82e3487a4596a6d
Anton Petrov <apetrov@ebi.ac.uk>
2015-03-13 16:52:07 +0000

    Switch to jsmol

---
+++ jsmol_initializer.js
@@ -0,0 +1,39 @@
+
+jmol_isReady = function(applet) {
+    // initialize the plugin
+    $('.jmolInline').jmolTools({
+        showStereoId: 'stereo',
+        showNeighborhoodId: 'neighborhood',
+        showNumbersId: 'showNtNums',
+        showNextId: 'next',
+        showPrevId: 'prev',
+        showAllId: 'all',
+        clearId: 'clear',
+        insertionsId: 'insertions'
+    });
+    // run the plugin
+    $('.jmolInline').first().jmolToggle();
+};
+
+var Info = {
+    width: 340,
+    height: 340,
+    debug: false,
+    color: 'white',
+    addSelectionOptions: false,
+    use: 'HTML5',
+    j2sPath: 'file:///Users/apetrov/Desktop/jmolTools/jsmol/j2s',
+    readyFunction: jmol_isReady,
+    disableInitialConsole: true
+};
+
+var jmolApplet0 = Jmol.getApplet('jmolApplet0', Info);
+
+// these are conveniences that mimic behavior of Jmol.js
+function jmolCheckbox(script1, script0,text,ischecked) {Jmol.jmolCheckbox(jmolApplet0,script1, script0, text, ischecked)};
+function jmolButton(script, text) {Jmol.jmolButton(jmolApplet0, script,text)};
+function jmolHtml(s) { document.write(s) };
+function jmolBr() { jmolHtml("<br />") };
+function jmolMenu(a) {Jmol.jmolMenu(jmolApplet0, a)};
+function jmolScript(cmd) {Jmol.script(jmolApplet0, cmd)};
+function jmolScriptWait(cmd) {Jmol.scriptWait(jmolApplet0, cmd)};
